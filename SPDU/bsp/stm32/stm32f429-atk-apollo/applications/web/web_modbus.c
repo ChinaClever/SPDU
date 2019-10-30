59cf96e19a05628c7937caa9dc9a5889be7f7710
luozhiyong <luozhiyong131@qq.com>
2019-10-30 13:07:03 +0800

    网络功能完成

--- SPDU/bsp/stm32/stm32f429-atk-apollo/applications/web/web_modbus.c
+++ SPDU/bsp/stm32/stm32f429-atk-apollo/applications/web/web_modbus.c
@@ -15,8 +15,6 @@ void web_modbus_get(struct webnet_session* session)
 	ptr[0] = 0;
 	sprintf(ptr, "en=%d", t->en); ptr += strlen(ptr);
 	sprintf(ptr, "&port=%d", t->port); ptr += strlen(ptr);
-	sprintf(ptr, "&addr=%d",t->addr); ptr += strlen(ptr);
-	sprintf(ptr, "&baud=%d",t->baud); ptr += strlen(ptr);
 
 	webnet_session_printf(session, "%s", pWebQuest);
 }
@@ -26,18 +24,14 @@ void web_modbus_set(struct webnet_session* session)
 	struct webnet_request* request = session->request;
 	const char *en_str = webnet_request_get_query(request, "en");
 	const char *port_str = webnet_request_get_query(request, "port");
-	const char *addr_str = webnet_request_get_query(request, "addr");
-	const char *baud_str = webnet_request_get_query(request, "baud");
 
 	struct sMdCfg t;
 	t.en = atoi(en_str);
 	t.port = atoi(port_str);
-	t.addr = atoi(addr_str);
-	t.baud = atoi(baud_str);
 	mb_cfg_set(&t);
 
-	static const char* status = "en=%d&port=%d&addr=%d&baud=%d";
-	webnet_session_printf(session, status, t.en, t.port, t.addr, t.baud);
+	static const char* status = "en=%d&port=%d";
+	webnet_session_printf(session, status, t.en, t.port);
 }
 
 
